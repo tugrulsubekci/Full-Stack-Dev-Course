29a71ea0d02de437875250b11ec935dec9543a60
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-02-18 11:41:45 +0300

    js intro and dom challange completed

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,15 @@
+// var x = document.getElementsByClassName("list");
+// for (let index = 0; index < x.length; index++) {
+//     const element = x[index];
+//     if(element.innerHTML === "Third")
+//         element.innerHTML ="siktir";
+// }
+
+// var x = document.querySelector("ul li a");
+// x.setAttribute("style", "color:red;")
+
+var clickMeBtn = document.querySelector("button").style.backgroundColor = "yellow";
+
+var header1 = document.querySelector("h1").classList.add("huge");
+
+console.log(header1);
---
+++ JS-Intro/index.js
@@ -0,0 +1,5 @@
+var elements = document.getElementsByClassName("deneme");
+for(var x = 0; x <elements.length; x++) {
+    elements[x].innerHTML = "kolaymis";
+}
+alert(elements.length);
