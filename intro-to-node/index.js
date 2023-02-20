066d4a63198dc196f2ff9df0ab74572d0e8a1c44
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-02-20 23:05:50 +0300

    intro to node
    install superheroes package
    install supervillains package

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var randomSuperHero = superheroes.random();
+var randomSuperVillian = supervillains.random();
+
+console.log(randomSuperHero);
+console.log(randomSuperVillian);
