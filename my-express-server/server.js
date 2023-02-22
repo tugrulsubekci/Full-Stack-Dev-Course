7e511a6bb479970715df2e54021a70138abf5c41
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-02-22 22:22:20 +0300

    Update server.js

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+var PORT = 3333;
+
+app.get('/', (req, res) => {  
+    res.send("<h1>HELLO MAN</h1>");
+})
+app.get('/contact', (req, res) => {  
+    res.send("my email address: tugrul@subekci.com");
+})
+app.get('/about', (req, res) => {  
+    res.send("I am a software developer");
+})
+app.get('/deneme', (req, res) => {  
+    res.send("deneme");
+})
+
 app.listen(PORT, function   () {
 console.log("App listening on PORT " + PORT);
 })
