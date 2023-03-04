ee7f8ebdff4e7388f04c9d4b800d38798ce72a90
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-03-04 12:53:05 +0300

    newsletter-signup project created

---
+++ Newsletter-SignUp/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
+
