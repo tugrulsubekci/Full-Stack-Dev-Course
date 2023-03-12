8a880c8817d695d1a91ea0ce6f1f6afaba5e6f96
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-03-12 21:34:26 +0300

    ejs - to do list app challange completed

--- todolist-app-v1/app.js
+++ todolist-app-v1/app.js
@@ -1,34 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-var items = [];
+let items = ["Go to home", "Go to school", "Go to work"];
+let newItems = [];
 
 app.get('/', (req, res) => {
-    var date = new Date();
-    var options = {
-        weekday : "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    }
+
+    let day = date.getDate();
     
     res.render('list', {
-        dayName: date.toLocaleDateString("en-US", options),
+        listTitle: day,
         newListItems: items
     });
 });
 
-app.post("/", function(req, res) {
-    var item = req.body.listItem;
-    items.push(item);
+app.get('/work', (req, res) => {
+    res.render('list', {
+        listTitle: "Work",
+        newListItems: newItems
+    });
+});
 
-    res.redirect("/");
+app.post("/", function(req, res) {
+    let item = req.body.listItem;
+
+    if(req.body.addListItem === "Work") {
+        newItems.push(item);
+        res.redirect("/work")
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
+
 })
 
 app.listen(3000, () => {
