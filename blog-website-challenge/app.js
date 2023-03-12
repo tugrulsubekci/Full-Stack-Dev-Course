51a77a273b8da608ff1c786dfff4ce8cfd81e5b0
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-03-13 00:04:26 +0300

    challanges completed from 0 to 9

--- blog-website-challenge/app.js
+++ blog-website-challenge/app.js
@@ -15,18 +15,31 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get('/', (req, res) => {
+  res.render("home.ejs", {
+    homeParagraph : homeStartingContent
+  });
+});
 
+app.get('/about', (req, res) => {
+  res.render("about.ejs", {
+    aboutParagraph : aboutContent
+  });
+});
 
+app.get('/contact', (req, res) => {
+  res.render("contact.ejs", {
+    contactParagraph : contactContent
+  });
+});
+app.get('/compose', (req, res) => {
+  res.render("compose.ejs");
+});
 
-
-
-
-
-
-
-
-
-
+app.post("/compose", (req, res) => {
+  console.log(req.body.titleText);
+  res.redirect("/compose");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
