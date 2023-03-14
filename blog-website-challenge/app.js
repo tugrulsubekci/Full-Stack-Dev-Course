9d59df0f47952b19596e43561bb69bdc6c3438eb
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-03-15 00:16:51 +0300

    blog website challange completed

--- blog-website-challenge/app.js
+++ blog-website-challenge/app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require('lodash');
+const { indexOf } = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -52,17 +54,22 @@ app.post("/compose", (req, res) => {
 });
 
 app.get("/posts/:postTitle", (req, res) => {
+  console.log(req.params.postTitle);
   let titles = [];
 
   postObjects.forEach((postObject) => {
-    titles.push(postObject.title);
+    let title = lodash.replace(lodash.lowerCase(postObject.title)," ", "-") ;
+    titles.push(title);
   });
 
-  if(titles.includes(req.params.postTitle)) {
-    console.log("Match found!");
-  }
-  else {
-    console.log("No match found!");
+  let param = lodash.replace(lodash.lowerCase(req.params.postTitle)," ", "-");
+  
+  if(titles.includes(param)) {
+    let index = lodash.indexOf(titles,param);
+    res.render("post", {
+      title : postObjects[index].title,
+      paragraph : postObjects[index].body
+    })
   }
 });
 
