bf6765e20a0381b0cb8235046a210f2a0cafdd9d
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-03-29 20:53:22 +0300

    mongoose section completed

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -2,21 +2,22 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB', {useNewUrlParser: true});
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number
-})
-
-const Person = mongoose.model('Person', personSchema);
-
 const fruitSchema = new mongoose.Schema({
-  name: String,
+  name: {type: String, required: true},
   rating: Number,
   review: String
 })
 
 const Fruit = mongoose.model('Fruit', fruitSchema);
-  
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFruit: fruitSchema
+})
+
+const Person = mongoose.model('Person', personSchema);
+
 // main();
 
 // async function main() {
@@ -43,15 +44,58 @@ const Fruit = mongoose.model('Fruit', fruitSchema);
 //   await orange.save();
 //   await banana.save();
 // };
-getFruits();
+// getFruits();
 
 
-async function getFruits() {
-  await Fruit.find().then((collection)=> {
-    collection.forEach((f)=> {
-      console.log(f.name);
-    })
-  });
+// async function getFruits() {
+//   await Fruit.find().then((collection)=> {
+//     collection.forEach((f)=> {
+//       console.log(f.name);
+//     })
+//   });
+  
+//   mongoose.connection.close();
+// }
+
+// saveBerries();
+
+// async function saveBerries() {
+//   const berries = new Fruit({
+//     review: "very good berriesssss"
+//   })
+
+//   berries.save();
+
+//   mongoose.connection.close();
+// }
+
+// deleteOne();
+
+// async function deleteOne() {
+//   await Person.deleteMany({name: "Deneme"}).then(function(deletedOne) {
+//     console.log(deletedOne);
+//   });
+
+//   mongoose.connection.close();
+// }
+establishRelationship();
+async function establishRelationship() {
+  // const pineapple = new Fruit({
+  //   name: "Pineapple",
+  //   rating: 100,
+  //   review:"From Turkey, greetings"
+  // })
+
+  // await pineapple.save();
+
+  // const ahmet = new Person({
+  //   name: "John",
+  //   age: 33,
+  // })
+
+  // await ahmet.save();
+  const pineapple = await Fruit.findOne({name: "Pineapple"});
+  await Person.updateOne({name: "John"}, {favoriteFruit: pineapple});
 
   mongoose.connection.close();
 }
