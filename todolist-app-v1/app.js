0e6058b1b35552da39d5e4085b3da5de118eb637
Tuğrul Sübekci <104980354+tugrulsubekci@users.noreply.github.com>
2023-03-30 00:57:04 +0300

    mongoose installed to todolistappv1

--- todolist-app-v1/app.js
+++ todolist-app-v1/app.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + "/date.js");
-
+const mongoose = require('mongoose');
+const { json } = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/todoListDB", {useNewUrlParser: true});
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
 app.set('view engine', 'ejs');
 
-let items = ["Go to home", "Go to school", "Go to work"];
+let items = [];
 let newItems = [];
 
-app.get('/', (req, res) => {
-
+app.get('/', async (req, res) => {
+    await getItemsDB();
     let day = date.getDate();
     
     res.render('list', {
@@ -37,11 +49,39 @@ app.post("/", function(req, res) {
         res.redirect("/work")
     }
     else {
+        let todoListItem = new Item({
+            name: item
+        })
+        
+        saveToDB(todoListItem);
+
         items.push(item);
         res.redirect("/");
     }
 
-})
+});
+
+app.post("/delete", async function(req, res) {
+    const id = req.body.checkbox;
+    await deleteByID(id);
+    res.redirect("/");
+});
+
+async function saveToDB(item) {
+    await item.save();
+}
+async function getItemsDB() {
+    const allItems = await Item.find();
+    console.log(allItems);
+    items = [];
+    allItems.forEach(function(item) {
+        items.push(item);
+    });
+}
+
+async function deleteByID(id) {
+    await Item.findByIdAndRemove(id);
+}
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000');
